aeaa97ded8117ddec7071403b3c71634f2c9910a
Chris Johnston <cjohnston@connected.io>
2022-10-18 10:44:11 -0400

    removes texttest test

--- Java-Approvals/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ Java-Approvals/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -15,24 +15,10 @@ public class GildedRoseApprovalTest {
 
 	@Test
 	public void foo() {
-
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
 		Approvals.verifyAll("Items", items);
 	}
-
-    @Test
-    public void thirtyDays() {
-
-        ByteArrayOutputStream fakeoutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(fakeoutput));
-        System.setIn(new ByteArrayInputStream("a\n".getBytes()));
-
-        Program.main();
-        String output = fakeoutput.toString();
-
-        Approvals.verify(output);
-    }
 }
