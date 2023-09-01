9cfc41c8981a7987dcce666cd3471ec1e7e2e4dc
Chris Johnston <cjohnston@connected.io>
2023-09-01 11:10:25 -0400

    ports ruby tests over for existing functionality

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -1,58 +1,98 @@
 defmodule GildedRose do
-  # Example
-  # update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
-  # => [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
+  @moduledoc """
+  Functions for the Gilded Rose Kata
+  """
 
+  @doc """
+  Update the quality of an item
+
+  ## Examples
+
+      iex> GildedRose.update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
+      [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}]
+
+  """
   def update_quality(items) do
     Enum.map(items, &update_item/1)
   end
 
+  @doc """
+  Updates the quality of an item
+
+  ## Examples
+
+      iex> GildedRose.update_item(%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1})
+      %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 3}
+
+  """
   def update_item(item) do
-    item = cond do
-      item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-        if item.quality > 0 do
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            %{item | quality: item.quality - 1}
+    item =
+      cond do
+        item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
+          if item.quality > 0 do
+            if item.name != "Sulfuras, Hand of Ragnaros" do
+              %{item | quality: item.quality - 1}
+            else
+              item
+            end
           else
             item
           end
-        else
+
+        true ->
+          cond do
+            item.quality < 50 ->
+              item = %{item | quality: item.quality + 1}
+
+              cond do
+                item.name == "Backstage passes to a TAFKAL80ETC concert" ->
+                  item =
+                    cond do
+                      item.sell_in < 11 ->
+                        cond do
+                          item.quality < 50 ->
+                            %{item | quality: item.quality + 1}
+
+                          true ->
+                            item
+                        end
+
+                      true ->
+                        item
+                    end
+
+                  cond do
+                    item.sell_in < 6 ->
+                      cond do
+                        item.quality < 50 ->
+                          %{item | quality: item.quality + 1}
+
+                        true ->
+                          item
+                      end
+
+                    true ->
+                      item
+                  end
+
+                true ->
+                  item
+              end
+
+            true ->
+              item
+          end
+      end
+
+    item =
+      cond do
+        item.name != "Sulfuras, Hand of Ragnaros" ->
+          %{item | sell_in: item.sell_in - 1}
+
+        true ->
           item
-        end
-      true ->
-        cond do
-          item.quality < 50 ->
-            item = %{item | quality: item.quality + 1}
-            cond do
-              item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                item = cond do
-                  item.sell_in < 11 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-                cond do
-                  item.sell_in < 6 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> item
-            end
-          true -> item
-        end
-    end
-    item = cond do
-      item.name != "Sulfuras, Hand of Ragnaros" ->
-        %{item | sell_in: item.sell_in - 1}
-      true -> item
-    end
+      end
+
     cond do
       item.sell_in < 0 ->
         cond do
@@ -64,20 +104,31 @@ defmodule GildedRose do
                     cond do
                       item.name != "Sulfuras, Hand of Ragnaros" ->
                         %{item | quality: item.quality - 1}
-                      true -> item
+
+                      true ->
+                        item
                     end
-                  true -> item
+
+                  true ->
+                    item
                 end
-              true -> %{item | quality: item.quality - item.quality}
+
+              true ->
+                %{item | quality: item.quality - item.quality}
             end
+
           true ->
             cond do
               item.quality < 50 ->
                 %{item | quality: item.quality + 1}
-              true -> item
+
+              true ->
+                item
             end
         end
-      true -> item
+
+      true ->
+        item
     end
   end
 end
--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -1,10 +1,266 @@
 defmodule GildedRoseTest do
+  @moduledoc """
+  Tests for Gilded Rose kata
+  """
   use ExUnit.Case
+  doctest GildedRose
 
-  test "begin the journey of refactoring" do
-    items = [%Item{name: "foo", sell_in: 0, quality: 0}]
-    GildedRose.update_quality(items)
-    %{name: firstItemName} = List.first(items)
-    assert "fixme" == firstItemName
+  # "Aged Brie"
+  # Backstage passes to a TAFKAL80ETC concert
+  # Sulfuras, Hand of Ragnaros
+  # Normal Item
+
+  # NORMAL ITEM
+  test "update quality with a single normal item before sell date" do
+    item = %Item{name: "NORMAL ITEM", sell_in: 5, quality: 10}
+
+    [%Item{name: "NORMAL ITEM", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 9 == quality
+    assert 4 == sell_in
+  end
+
+  test "update quality with a single normal item on sell date" do
+    item = %Item{name: "NORMAL ITEM", sell_in: 0, quality: 10}
+
+    [%Item{name: "NORMAL ITEM", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 8 == quality
+    assert -1 == sell_in
+  end
+
+  test "update quality with a single normal item after sell date" do
+    item = %Item{name: "NORMAL ITEM", sell_in: -10, quality: 10}
+
+    [%Item{name: "NORMAL ITEM", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 8 == quality
+    assert -11 == sell_in
+  end
+
+  test "update quality with a single normal item of zero quality" do
+    item = %Item{name: "NORMAL ITEM", sell_in: 5, quality: 0}
+
+    [%Item{name: "NORMAL ITEM", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 0 == quality
+    assert 4 == sell_in
+  end
+
+  # Aged Brie
+  test "update quality with a single aged brie item before sell date" do
+    item = %Item{name: "Aged Brie", sell_in: 5, quality: 10}
+
+    [%Item{name: "Aged Brie", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 11 == quality
+    assert 4 == sell_in
+  end
+
+  test "update quality with a single aged brie item before sell date with max quality" do
+    item = %Item{name: "Aged Brie", sell_in: 5, quality: 50}
+
+    [%Item{name: "Aged Brie", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 50 == quality
+    assert 4 == sell_in
+  end
+
+  test "update quality with a single aged brie item before on sell date" do
+    item = %Item{name: "Aged Brie", sell_in: 0, quality: 10}
+
+    [%Item{name: "Aged Brie", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 12 == quality
+    assert -1 == sell_in
+  end
+
+  test "update quality with a single aged brie item before on sell date near max quality" do
+    item = %Item{name: "Aged Brie", sell_in: 0, quality: 49}
+
+    [%Item{name: "Aged Brie", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 50 == quality
+    assert -1 == sell_in
+  end
+
+  test "update quality with a single aged brie item before on sell date with max quality" do
+    item = %Item{name: "Aged Brie", sell_in: 0, quality: 50}
+
+    [%Item{name: "Aged Brie", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 50 == quality
+    assert -1 == sell_in
+  end
+
+  test "update quality with a single aged brie item after sell date" do
+    item = %Item{name: "Aged Brie", sell_in: -10, quality: 10}
+
+    [%Item{name: "Aged Brie", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 12 == quality
+    assert -11 == sell_in
+  end
+
+  test "update quality with a single aged brie item after sell date with max quality" do
+    item = %Item{name: "Aged Brie", sell_in: -10, quality: 50}
+
+    [%Item{name: "Aged Brie", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 50 == quality
+    assert -11 == sell_in
+  end
+
+  # Sulfuras
+  test "update quality with a single Sulfuras item before sell date" do
+    item = %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 5, quality: 80}
+
+    [%Item{name: "Sulfuras, Hand of Ragnaros", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 80 == quality
+    assert 5 == sell_in
+  end
+
+  test "update quality with a single Sulfuras item on sell date" do
+    item = %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 5, quality: 80}
+
+    [%Item{name: "Sulfuras, Hand of Ragnaros", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 80 == quality
+    assert 5 == sell_in
+  end
+
+  test "update quality with a single Sulfuras item after sell date" do
+    item = %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 5, quality: 80}
+
+    [%Item{name: "Sulfuras, Hand of Ragnaros", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 80 == quality
+    assert 5 == sell_in
+  end
+
+  # Backstage passes to a TAFKAL80ETC concert
+  test "update quality with a single backstage Pass item long before sell date" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 11, quality: 10}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 11 == quality
+    assert 10 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item medium close to sell date (upper bound)" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 10}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 12 == quality
+    assert 9 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item medium close to sell date (upper bound) at max quality" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 50}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 50 == quality
+    assert 9 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item medium close to sell date (lower bound)" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 6, quality: 10}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 12 == quality
+    assert 5 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item medium close to sell date (lower bound) at max quality" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 6, quality: 50}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 50 == quality
+    assert 5 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item very close to sell date (upper bound)" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 10}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 13 == quality
+    assert 4 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item very close to sell date (upper bound) at max quality" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 50}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 50 == quality
+    assert 4 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item very close to sell date (lower bound)" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 1, quality: 10}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 13 == quality
+    assert 0 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item very close to sell date (lower bound) at max quality" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 1, quality: 50}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 50 == quality
+    assert 0 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item very on sell date" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 0, quality: 10}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 0 == quality
+    assert -1 == sell_in
+  end
+
+  test "update quality with a single backstage Pass item very after sell date" do
+    item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: -10, quality: 10}
+
+    [%Item{name: "Backstage passes to a TAFKAL80ETC concert", quality: quality, sell_in: sell_in}] =
+      GildedRose.update_quality([item])
+
+    assert 0 == quality
+    assert -11 == sell_in
   end
 end
